7173123261777b860cd6c1603e500d0154475287
SmartPolarBear <CleverCoolBear@outlook.com>
2022-04-07 23:26:41 +0800

    finish some report

--- integral/romberg.py
+++ integral/romberg.py
@@ -29,3 +29,37 @@ def integrate(f: Callable[[np.float], np.float], a: np.float, b: np.float, epsil
             return r[k, 0]
 
     return r[n, 0]
+
+
+def integrate2(f: Callable[[np.float], np.float], a: np.float, b: np.float, epsilon: np.float = 0.001,
+               n: int = 32):
+    r: np.ndarray = np.zeros((n + 1, n + 1), dtype=float)
+    h: np.float = b - a
+    r[0, 0] = 0.5 * h * (f(a) + f(b))
+
+    iters = []
+    vals = []
+
+    p2: int = 1
+    for k in range(1, n + 1):
+        h *= 0.5
+        p2 *= 2
+
+        r[k, 0] = 0.0
+        for i in range(1, p2, 2):
+            r[k, 0] += f(a + i * h)
+        r[k, 0] *= h
+        r[k, 0] += 0.5 * r[k - 1, 0]
+
+        p4 = 1
+        for m in range(1, k + 1):
+            p4 *= 4
+            r[k, m] = r[k, (m - 1)] + (r[k, (m - 1)] - r[k - 1, (m - 1)]) / (p4 - 1)
+
+        iters.append(k)
+        vals.append(r[k, 0])
+
+        if k != 1 and np.abs(r[k, 0] - r[(k - 1), 0]) < epsilon:
+            return r[k, 0], iters, vals
+
+    return r[n, 0], iters, vals
--- main.py
+++ main.py
@@ -6,30 +6,50 @@ import timeit
 
 import numpy as np
 
-from integral.romberg import integrate as i1
-from integral.montecarlo import integrate as i2
-from integral.qmc import integrate as i3
+# from integral.romberg import integrate as i1
+# from integral.montecarlo import integrate as i2
+# from integral.qmc import integrate as i3
+
+import matplotlib.pyplot as plt
+from integral.romberg import integrate2
 
 
 def f(x):
     return np.cos(x)
 
 
-start = timeit.default_timer()
-for i in range(1, 10):
-    i1(f, 0, np.pi / 2, 0.0001, 128)
-print(timeit.default_timer() - start)
+def f2(x):
+    return np.power(15 * np.power(x, 3) + 21 * np.power(x, 2) + 41 * x + 3, 1.0 / 4.0) * np.exp(-0.5 * x)
 
-start = timeit.default_timer()
-for i in range(1, 10):
-    i2(f, 0, np.pi / 2, 8192)
-print(timeit.default_timer() - start)
 
-start = timeit.default_timer()
-for i in range(1, 10):
-    i3(f, 0, np.pi / 2, 8192)
-print(timeit.default_timer() - start)
+def test_cov(ff, real, a, b):
+    val, iters, vals = integrate2(ff, a, b, 0.0001, 128)
 
-print(i1(f, 0, np.pi / 2, 0.0001, 128),
-      i2(f, 0, np.pi / 2, 16384),
-      i3(f, 0, np.pi / 2, 8192))
+    print(val)
+    plt.plot(iters, vals, 'ro-', [0, len(iters)], [real, real], 'b')
+    plt.xlabel('Iteration')
+    plt.ylabel('Integral Value')
+    plt.show()
+
+
+test_cov(f, 1, 0, np.pi / 2)
+test_cov(f2, 5.7674, 0, 4)
+
+# start = timeit.default_timer()
+# for i in range(1, 10):
+#     i1(f, 0, np.pi / 2, 0.0001, 128)
+# print(timeit.default_timer() - start)
+#
+# start = timeit.default_timer()
+# for i in range(1, 10):
+#     i2(f, 0, np.pi / 2, 8192)
+# print(timeit.default_timer() - start)
+#
+# start = timeit.default_timer()
+# for i in range(1, 10):
+#     i3(f, 0, np.pi / 2, 8192)
+# print(timeit.default_timer() - start)
+#
+# print(i1(f, 0, np.pi / 2, 0.0001, 128),
+#       i2(f, 0, np.pi / 2, 16384),
+#       i3(f, 0, np.pi / 2, 8192))
