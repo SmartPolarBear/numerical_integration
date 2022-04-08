ac4013004052f11776c3a3ef7f8ca66c9454d143
SmartPolarBear <CleverCoolBear@outlook.com>
2022-04-08 16:04:03 +0800

    finish graphs for report

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import timeit
 
 import numpy as np
 
-# from integral.romberg import integrate as i1
-# from integral.montecarlo import integrate as i2
-# from integral.qmc import integrate as i3
+from integral.romberg import integrate as i1
+from integral.montecarlo import integrate as i2
+from integral.qmc import integrate as i3
 
 import matplotlib.pyplot as plt
 from integral.romberg import integrate2
@@ -32,24 +32,102 @@ def test_cov(ff, real, a, b):
     plt.show()
 
 
-test_cov(f, 1, 0, np.pi / 2)
-test_cov(f2, 5.7674, 0, 4)
+def test_speed():
+    rm = []
+    mc = []
+    qmc = []
 
-# start = timeit.default_timer()
-# for i in range(1, 10):
-#     i1(f, 0, np.pi / 2, 0.0001, 128)
-# print(timeit.default_timer() - start)
-#
-# start = timeit.default_timer()
-# for i in range(1, 10):
-#     i2(f, 0, np.pi / 2, 8192)
-# print(timeit.default_timer() - start)
-#
-# start = timeit.default_timer()
-# for i in range(1, 10):
-#     i3(f, 0, np.pi / 2, 8192)
-# print(timeit.default_timer() - start)
-#
-# print(i1(f, 0, np.pi / 2, 0.0001, 128),
-#       i2(f, 0, np.pi / 2, 16384),
-#       i3(f, 0, np.pi / 2, 8192))
+    start = timeit.default_timer()
+    for i in range(1, 1000):
+        i1(f, 0, np.pi / 2, 0.0001, 128)
+    rm.append((timeit.default_timer() - start) / 1000)
+
+    start = timeit.default_timer()
+    for i in range(1, 1000):
+        i1(f2, 0, 4, 0.0001, 128)
+    rm.append((timeit.default_timer() - start) / 1000)
+
+    start = timeit.default_timer()
+    for i in range(1, 1000):
+        i2(f, 0, np.pi / 2, 8192)
+    mc.append((timeit.default_timer() - start) / 1000)
+
+    start = timeit.default_timer()
+    for i in range(1, 1000):
+        i2(f2, 0, 4, 8192)
+    mc.append((timeit.default_timer() - start) / 1000)
+
+    start = timeit.default_timer()
+    for i in range(1, 1000):
+        i3(f, 0, np.pi / 2, 8192)
+    qmc.append((timeit.default_timer() - start) / 1000)
+
+    start = timeit.default_timer()
+    for i in range(1, 1000):
+        i3(f2, 0, 4, 8192)
+    qmc.append((timeit.default_timer() - start) / 1000)
+
+    x = np.arange(2)
+    total_width, n = 0.8, 3
+    width = total_width / n
+    x = x - (total_width - width) / 2
+
+    tick_labels = ["f(x)", "g(x)"]
+    plt.bar(x, rm, width=width, label='Romberg')
+    plt.bar(x + width, mc, width=width, label='Monte Carlo')
+    plt.bar(x + 2 * width, qmc, width=width, label='Quasi-Monte Carlo')
+    plt.legend()
+    plt.xticks(x + width / 2, tick_labels)
+    plt.show()
+
+
+def test_variance():
+    iters = []
+    vals1 = []
+    vals2 = []
+    for i in range(1, 500):
+        iters.append(i)
+        vals1.append(i2(f2, 0, 4, 8192))
+        vals2.append(i3(f2, 0, 4, 8192))
+
+    print(np.mean(np.array(vals1, dtype=float)))
+    print(np.var(np.array(vals1, dtype=float)))
+    print(np.mean(np.array(vals2, dtype=float)))
+    print(np.var(np.array(vals2, dtype=float)))
+
+    real = 5.767433490695931
+    plt.plot(iters, vals1, 'ro', iters, vals2, 'go', [0, len(iters)], [real, real], 'b')
+    plt.xlabel('Iteration')
+    plt.ylabel('Integral Value')
+    plt.show()
+
+    plt.plot(iters, vals1, 'ro', [0, len(iters)], [real, real], 'b')
+    plt.xlabel('Iteration')
+    plt.ylabel('Integral Value')
+    plt.show()
+
+    plt.plot(iters, vals2, 'go', [0, len(iters)], [real, real], 'b')
+    plt.xlabel('Iteration')
+    plt.ylabel('Integral Value')
+    plt.show()
+
+test_variance()
+
+start = timeit.default_timer()
+for i in range(1, 1000):
+    i1(f2, 0, 4, 0.0001, 128)
+print(timeit.default_timer() - start)
+
+start = timeit.default_timer()
+for i in range(1, 10):
+    i2(f, 0, np.pi / 2, 8192)
+print(timeit.default_timer() - start)
+
+start = timeit.default_timer()
+for i in range(1, 1000):
+    i3(f2, 0, 4, 8192)
+print(timeit.default_timer() - start)
+
+print(i1(f, 0, np.pi / 2, 0.0001, 128),
+      i2(f, 0, np.pi / 2, 16384),
+      i3(f, 0, np.pi / 2, 8192))
